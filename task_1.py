eac40984806ac3fae05d1de26ad5630ffac02914
Max Samsonenko <samsonenko.maxwork@gmail.com>
2025-03-03 15:19:14 +0200

    complete

---
+++ task_1.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    def __init__(self, make, model, region):
+        self.make = make
+        self.model = model
+        self.region = region 
+
+    @abstractmethod
+    def start_engine(self):
+        pass
+
+class Car(Vehicle):
+    def start_engine(self):
+        print(f"{self.make} {self.model} ({self.region} Spec): Двигун запущено")
+
+class Motorcycle(Vehicle):
+    def start_engine(self):
+        print(f"{self.make} {self.model} ({self.region} Spec): Мотор заведено")
+
+class VehicleFactory(ABC):
+    @abstractmethod
+    def create_car(self, make, model):
+        pass
+
+    @abstractmethod
+    def create_motorcycle(self, make, model):
+        pass
+
+class USVehicleFactory(VehicleFactory):
+    def create_car(self, make, model):
+        return Car(make, model, "US")
+
+    def create_motorcycle(self, make, model):
+        return Motorcycle(make, model, "US")
+
+class EUVehicleFactory(VehicleFactory):
+    def create_car(self, make, model):
+        return Car(make, model, "EU")
+
+    def create_motorcycle(self, make, model):
+        return Motorcycle(make, model, "EU")
+
+us_factory = USVehicleFactory()
+eu_factory = EUVehicleFactory()
+
+vehicle1 = us_factory.create_car("Ford", "Mustang")
+vehicle1.start_engine()  
+
+vehicle2 = eu_factory.create_motorcycle("BMW", "R1250")
+vehicle2.start_engine()
+
+# vehicle = Car("Toyota", "Corolla", "US")
+# print(vehicle.start_engine()) 
